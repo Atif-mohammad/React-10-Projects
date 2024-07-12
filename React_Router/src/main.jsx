88517e94a298dd91be13770c90210801eb1aabb3
Atif-mohammad <sy24604@gmail.com>
2024-07-12 18:24:51 +0530

    React Browser Navigation done

---
+++ React_Router/src/main.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { createBrowserRouter, createRoutesFromElements, RouterProvider,Route } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import About from './components/About/About.jsx'
+import User from './components/User/User.jsx'
+import Github from './components/Github/Github.jsx'
+import Home from './components/Home/Home.jsx'
+
+
+const router = createBrowserRouter(createRoutesFromElements(
+
+  <Route path='/' element={<Layout/ >}> 
+    <Route path='about' element={<About/>}></Route>
+    <Route path='user' element={<User/>}></Route>
+    <Route path='home' element={<Home/>}></Route>
+    <Route path='github' element={<Github/>}></Route>
+
+  </Route>
+))
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
